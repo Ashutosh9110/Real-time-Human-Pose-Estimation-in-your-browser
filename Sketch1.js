6e5f1530f6c0cf1a1d60907b4cf2c71826d352c7
Ashutosh9110 <113494449+Ashutosh9110@users.noreply.github.com>
2023-05-26 13:10:49 +0530

    Update Sketch1.js

--- Sketch1.js
+++ Sketch1.js
@@ -1,11 +1,11 @@
 let capture;
 let posenet;
-let noseX,noseY;
-let reyeX,reyeY;
-let leyeX,leyeY;
-let singlePose,skeleton;
-let actor_img;
-let specs,smoke;
+let noseX, noseY; 
+let reyeX, reyeY;
+let leyeX, leyeY;
+let singlePose, skeleton;
+let actor_img; 
+let specs, smoke;
 
 function setup() {
     createCanvas(800, 500);
@@ -13,44 +13,48 @@ function setup() {
     capture.hide();
 
     posenet = ml5.poseNet(capture, modelLoaded);
-    posenet.on('pose',receivedPoses);
+    posenet.on("pose", receivedPoses);  
 
-    actor_img = loadImage('images/shahrukh.png');
-    specs = loadImage('images/spects.png');
-    smoke = loadImage('images/cigar.png');
-
+    actor_img = loadImage("images/Shahruk.png");
+    specs = loadImage("images/specs.png");
+    smoke = loadImage("images/cigar.png");
 }
+
 
 function receivedPoses(poses){
     console.log(poses);
 
-    if(poses.length > 0){
-        singlePose = poses[0].pose;
+    if (poses.length > 0){ 
+        singlePose = poses[0].pose; 
         skeleton = poses[0].skeleton;
-    }
-}
 
-function modelLoaded() {
-    console.log('Model has loaded');
+
+    }
+
 }
+function modelLoaded() {
+    console.log("Model has loaded");
+
+}    
+
 
 function draw() {
 
-    // images and videos(webcam)
-    image(capture, 0, 0);
-    fill(255,0,0);
+    image(capture, 0, 0); 
+    fill(255, 0, 0);
+ 
 
     if(singlePose){
-        for(let i=0; i<singlePose.keypoints.length; i++){
-            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y,20);
+        for(let i = 0; i < singlePose.keypoints.length; i++){ // this loop will run for 17 times.
+            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y, 20);
         }
-        stroke(255,255,255);
-        strokeWeight(5);
-        for(let j=0; j<skeleton.length; j++){
-            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y)
-        }
+        stroke(255, 255, 255); 
+        strokeWeight(5); 
+        for(let j = 0; j < skeleton.length; j++){
+            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y) 
 
-        //image(specs,singlePose.nose.x-35,singlePose.nose.y-50,80,80);
-        //image(smoke,singlePose.nose.x-35,singlePose.nose.y+10,40,40);
+        }
+        // image(specs, singlePose.nose.x-55, singlePose.nose.y-100, 120, 120);// calling image function out of for loop
+        // image(smoke, singlePose.nose.x-35, singlePose.nose.y +35, 35, 70);
     }
 }
